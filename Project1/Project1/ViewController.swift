f8ef351d6b773dedc5cf3000186be0c8c1fbe272
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2019-03-13 23:09:10 -0400

    Day 16
    Project 1
    - Setting up
    - Listing images with FileManager
    - Designing our interface

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Luis Eduardo Gonzalez on 2019-03-13.
+//  Copyright © 2019 Luis Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Let us work with the filesystem
+        let fileManager = FileManager.default
+
+        // Resource path of our app's bundle
+        // Bundle: directory containing our compiled program and all our assets
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
