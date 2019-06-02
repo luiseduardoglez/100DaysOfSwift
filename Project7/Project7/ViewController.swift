41edab3e3b712aeafa2cc0e8678d5a1bee8c7296
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2019-06-02 03:21:58 -0400

    Finished day 35

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ViewController: UITableViewController {
 
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let creditButton = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        let filterButton = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filter))
+        navigationItem.leftBarButtonItems = [creditButton]
+        navigationItem.rightBarButtonItems = [filterButton]
 
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         let urlString: String
@@ -27,6 +33,7 @@ class ViewController: UITableViewController {
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                filteredPetitions = petitions
                 return
             }
         }
@@ -35,12 +42,12 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -48,7 +55,7 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 
@@ -64,6 +71,30 @@ class ViewController: UITableViewController {
     func showError() {
         let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "This data come from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
+    @objc func filter() {
+        let ac = UIAlertController(title: "Filter", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let okAction = UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            if answer == "" {
+                self?.filteredPetitions = self?.petitions ?? []
+            } else {
+                self?.filteredPetitions = self?.petitions.filter { $0.body.contains(answer)} ?? []
+            }
+            self?.tableView.reloadData()
+        }
+
+        ac.addAction(okAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
     }
 }
