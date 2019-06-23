20a8a3eb0c1f024ea1d6e51c59657aa4753dd20b
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2019-06-23 18:06:14 -0400

    Day 42

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Luis Eduardo Gonzalez on 2019-06-23.
+//  Copyright © 2019 Luis Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
