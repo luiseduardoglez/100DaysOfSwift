4a876a7bba4e9fcfee02c06a5ee8279bc66f53a7
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2019-08-22 19:55:10 -0400

    Day 52

---
+++ project13/project13/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  project13
+//
+//  Created by Luis Eduardo Gonzalez on 2019-07-23.
+//  Copyright © 2019 Luis Eduardo Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    var currentImage: UIImage!
+    @IBOutlet weak var intensity: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        dismiss(animated: true)
+
+        currentImage = image
+    }
+
+}
+
